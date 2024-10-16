9056ace014c7079e2d73c3ad82a65acc9bc61d8c
ivankhpm <167341183+ivankhpm@users.noreply.github.com>
2024-10-16 17:42:26 +0300

    Update Task4.3.py

--- Task4.3.py
+++ Task4.3.py
@@ -1,20 +1,38 @@
 
 
-def normalize_text(text):
+# def normalize_text(text):
+#     sentences = text.split('.')
+#     # Trim spaces and capitalize the first letter of each sentence
+#     capitalized_sentences = [sentence.strip().capitalize() for sentence in sentences if sentence.strip()]
+#     last_words = [capitalized_sentences.strip().split()[-1] for capitalized_sentences in capitalized_sentences if capitalized_sentences.strip()]
+#     new_sentence = ' '.join(last_words) + '.'
+#
+#     # Join the sentences back into a single string with a period and space
+#     normalized_text = '.\n'.join(capitalized_sentences) + '.' + '\n' + new_sentence.capitalize()
+#
+#     # replace ' iz ' by ' is '
+#     normalized_text = normalized_text.replace(' iz ' , ' is ' )
+#     return normalized_text
+
+
+def capitalized_sentences(text):
     sentences = text.split('.')
-    # Trim spaces and capitalize the first letter of each sentence
-    capitalized_sentences = [sentence.strip().capitalize() for sentence in sentences if sentence.strip()]
-    last_words = [capitalized_sentences.strip().split()[-1] for capitalized_sentences in capitalized_sentences if capitalized_sentences.strip()]
-    new_sentence = ' '.join(last_words) + '.'
-
-    # Join the sentences back into a single string with a period and space
-    normalized_text = '.\n'.join(capitalized_sentences) + '.' + '\n' + new_sentence.capitalize()
-
-    # replace ' iz ' by ' is '
-    normalized_text = normalized_text.replace(' iz ' , ' is ' )
+    capitalized = [sentence.strip().capitalize() for sentence in sentences if sentence.strip()]
+    normalized_text = '.\n'.join(capitalized) + '.'
     return normalized_text
 
-## count whitespaces
+def create_last_word_sentence(text):
+    sentences = text.split('.')
+    last_words = [sentences.strip().split()[-1] for sentences in sentences if sentences.strip()]
+    new_sentence = (' '.join(last_words) + '.').capitalize()
+    return new_sentence
+
+
+def replace_iz(text):
+    text = text.replace(' iz ' , ' is ' )
+    return text
+
+
 def count_whitespaces(text):
     whitespaces = 0
     for symbol in text:
@@ -44,5 +62,8 @@ last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not 
 """
 
 
-print('NORMALIZED_TEXT:\n', normalize_text(hm3))
+print('Capitalized text:\n', capitalized_sentences(hm3))
+print('Replaced iz to is:\n', replace_iz(hm3))
+print('Last word sentence text:\n', create_last_word_sentence(hm3))
 print('\n\nMy own calculated value of whitespaces is', count_whitespaces(hm3))
+
